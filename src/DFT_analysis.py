785253e59e1c7b6af78cd3436dd7e9f97498abbf
Pratik Tambe <enthusiasticgeek@gmail.com>
2023-11-19 13:22:56 -0500

    exceptions try catch block added for DFT

--- src/DFT_analysis.py
+++ src/DFT_analysis.py
@@ -6,38 +6,46 @@ import argparse
 from datetime import datetime
 
 def reconstruct_image(image_path):
-    # Read the image as grayscale
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    try:
+        # Read the image as grayscale
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
-    # Apply DFT to the image
-    dft = cv2.dft(np.float32(image), flags=cv2.DFT_COMPLEX_OUTPUT)
-    dft_shift = np.fft.fftshift(dft)
+        # Apply DFT to the image
+        dft = cv2.dft(np.float32(image), flags=cv2.DFT_COMPLEX_OUTPUT)
+        dft_shift = np.fft.fftshift(dft)
 
-    # Compute magnitude spectrum (optional for visualization)
-    magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+        # Compute magnitude spectrum (optional for visualization)
+        magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
 
-    # Reconstruct the image using inverse DFT
-    fshift = np.fft.ifftshift(dft_shift)
-    reconstructed_image_complex = cv2.idft(fshift)
-    reconstructed_image = cv2.magnitude(reconstructed_image_complex[:, :, 0], reconstructed_image_complex[:, :, 1])
+        # Reconstruct the image using inverse DFT
+        fshift = np.fft.ifftshift(dft_shift)
+        reconstructed_image_complex = cv2.idft(fshift)
+        reconstructed_image = cv2.magnitude(reconstructed_image_complex[:, :, 0], reconstructed_image_complex[:, :, 1])
 
-    # Normalize and convert the reconstructed image to uint8
-    reconstructed_image = cv2.normalize(reconstructed_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        # Normalize and convert the reconstructed image to uint8
+        reconstructed_image = cv2.normalize(reconstructed_image, None, 0, 255, cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-    # Display the reconstructed image and magnitude spectrum
-    cv2.imshow('Magnitude Spectrum', magnitude_spectrum.astype(np.uint8))
-    cv2.imshow('Reconstructed Image', reconstructed_image)
+        # Display the reconstructed image and magnitude spectrum
+        cv2.imshow('Magnitude Spectrum', magnitude_spectrum.astype(np.uint8))
+        cv2.imshow('Reconstructed Image', reconstructed_image)
 
-    # Get current timestamp
-    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        # Get current timestamp
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
 
-    # Formulate the filename with timestamp
-    filename = f"magnitude_spectrum_{current_time}.png"
+        # Formulate the filename with timestamp
+        filename = f"magnitude_spectrum_{current_time}.png"
 
-    # Save the magnitude spectrum image
-    cv2.imwrite(filename, magnitude_spectrum)
-    cv2.waitKey(5000)
-    cv2.destroyAllWindows()
+        # Save the magnitude spectrum image
+        cv2.imwrite(filename, magnitude_spectrum)
+        cv2.waitKey(5000)
+        cv2.destroyAllWindows()
+
+    except cv2.error as e:
+        print(f"OpenCV error occurred: {e}")
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     # Parse command-line arguments
